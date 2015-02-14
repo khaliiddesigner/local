4bb6e945870dd5b1df6cbaee198240afbe2bdc85
khaliiddesigner <kha.liid.designer@gmail.com>
2015-02-14 21:53:02 +0000

    Create index.php

---
+++ index.php
@@ -0,0 +1,129 @@
+<html dir='rtl'>
+	
+	<head>
+		<title>4Tech</title>
+		<meta charset='utf-8'/>
+		<link href='style.css' rel='stylesheet' type='text/css'/>
+		<link href='images/ico.png' rel='icon' type='image/x-ico'/>
+	</head>
+	<body>
+		<header>
+			<div class='p'>
+				<p align='center'>مرحبا بك في 4Tech,  موقع عربي يهتم بالتكنولوجيا والمعلوميات.</p>
+			</div>
+			<div class='logo'>
+				<a href='index.php'>
+					<img title='4Tech' src='images/logo.png'/>
+				</a>
+			</div>
+			<nav class='nav'>
+				<ul>
+					<a class='b home' href='#' title='الرئيسية'> الرئيسية</a>
+					<a href='#'>أخبار</a>
+					<a href='#'>حلقات</a>
+					<a href='#'>تطبيقات</a>
+					<a href='#'>هاكر</a>
+				</ul>
+			</nav>
+			<form dir='rtl'action='#' method='GET'>
+					<input name='q' class='search' placeholder='البحث...'/>
+					<input type='submit' name='search' class='b se' value=' '/>
+			</form>
+			<div style='clear:both'/>
+		</header>
+		<ul class='body'>
+		<table class='center' align='center'>
+			<div class='contents' width='1000px'>
+				<td class='right' width='67%px'>
+					<tr class='posts'>
+						<table class='post'>
+							<td/>
+							<td width='100%'><a href='#' title='#'><h4 class='title'>مثال لعنوان يمكن استبداله في نفس المساحة</h4></a></td>
+							<div style='clear:both'/>
+							<tr>
+								<td width='30%'><a href='#' title='#'><img width='190px' height='190px' style='border-radius:10px;' src='images/data.png' /></a></td>
+								<td class='pstcon' width='50%'>
+									<p class='text'>
+										مثال لنص أو موضوع معين يمكن استبداله في نفس المساحة تم توليد
+										هذا النص من طرف فورتك... مثال لنص أو موضوع معين يمكن استبداله
+										في نفس المساحة تم توليد هذا النص من طرف فورتك... مثال لنص أو 
+										موضوع معين يمكن استبداله في نفس المساحة تم توليد هذا النص من 
+										طرف فورتك... مثال لنص أو موضوع معين يمكن استبداله في نفس 
+										المساحة تم توليد هذا النص من طرف فورتك... مثال لنص أو موضوع ...
+									</p>
+									<table class='plus'><tr>
+										<td><span class='admin'>من طرف : <a href='#'>KhaLiid</a></span></td>
+										<td><span class='date'>11 / 02 / 2015</span></td>
+										<td><a class='id' href='#'>أتمم القراءة</a></td>
+									</tr></table>
+								</td>
+							</tr>
+						</table>
+						<table class='post'>
+							<td/>
+							<td width='100%'><a href='#' title='#'><h4 class='title'>مثال لعنوان يمكن استبداله في نفس المساحة</h4></a></td>
+							<div style='clear:both'/>
+							<tr>
+								<td width='30%'><a href='#' title='#'><img width='190px' height='190px' style='border-radius:10px;' src='images/data.png' /></a></td>
+								<td class='pstcon' width='50%'>
+									<p class='text'>
+										مثال لنص أو موضوع معين يمكن استبداله في نفس المساحة تم توليد
+										هذا النص من طرف فورتك... مثال لنص أو موضوع معين يمكن استبداله
+										في نفس المساحة تم توليد هذا النص من طرف فورتك... مثال لنص أو 
+										موضوع معين يمكن استبداله في نفس المساحة تم توليد هذا النص من 
+										طرف فورتك... مثال لنص أو موضوع معين يمكن استبداله في نفس 
+										المساحة تم توليد هذا النص من طرف فورتك... مثال لنص أو موضوع ...
+									</p>
+									<table class='plus'><tr>
+										<td><span class='admin'>من طرف : <a href='#'>KhaLiid</a></span></td>
+										<td><span class='date'>11 / 02 / 2015</span></td>
+										<td><a class='id' href='#'>أتمم القراءة</a></td>
+									</tr></table>
+								</td>
+							</tr>
+						</table>
+						<table class='post'>
+							<td/>
+							<td width='100%'><a href='#' title='#'><h4 class='title'>مثال لعنوان يمكن استبداله في نفس المساحة</h4></a></td>
+							<div style='clear:both'/>
+							<tr>
+								<td width='30%'><a href='#' title='#'><img width='190px' height='190px' style='border-radius:10px;' src='images/data.png' /></a></td>
+								<td class='pstcon' width='50%'>
+									<p class='text'>
+										مثال لنص أو موضوع معين يمكن استبداله في نفس المساحة تم توليد
+										هذا النص من طرف فورتك... مثال لنص أو موضوع معين يمكن استبداله
+										في نفس المساحة تم توليد هذا النص من طرف فورتك... مثال لنص أو 
+										موضوع معين يمكن استبداله في نفس المساحة تم توليد هذا النص من 
+										طرف فورتك... مثال لنص أو موضوع معين يمكن استبداله في نفس 
+										المساحة تم توليد هذا النص من طرف فورتك... مثال لنص أو موضوع ...
+									</p>
+									<table class='plus'><tr>
+										<td><span class='admin'>من طرف : <a href='#'>KhaLiid</a></span></td>
+										<td><span class='date'>11 / 02 / 2015</span></td>
+										<td><a class='id' href='#'>أتمم القراءة</a></td>
+									</tr></table>
+								</td>
+							</tr>
+						</table>
+					</tr>
+				</td>
+				<div style='clear:both'/>
+				<td class='left' width='30%' style='text-align:center;margin-right:auto;margin-left:auto'>
+					<ul class='widgets'>
+							<div class='gadget'>
+								<div>
+									<h3>مختارات</h3>
+								</div>
+								<ul>
+									<td><a href='#'><h4>مثال لعنوان يمكن استبداله في نفس المساحة</h4></a></td>
+									<td><a href='#'><img src='images/data.png' width='70px' height='70px' style='border-radius:10px'/></a></td>
+								</ul>
+							</div>
+					</ul>
+				</td>
+			</div>
+		</table>
+		</ul>
+	</body>
+
+</html>
